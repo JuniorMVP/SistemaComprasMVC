a9588ce3d4bf91a07a58c2865ad96fa368818b7e
JuniorMVP <cesarjunior1212@gmail.com>
2024-12-01 01:34:12 -0400

    se realizo migracion para crear db

--- Migrations/20241130045705_AgregarOrdenDeCompra.Designer.cs
+++ Migrations/20241130045705_AgregarOrdenDeCompra.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaComprasMVC.Data;
 
@@ -11,9 +12,10 @@ using SistemaComprasMVC.Data;
 namespace SistemaComprasMVC.Migrations
 {
     [DbContext(typeof(SistemaComprasContext))]
-    partial class SistemaComprasContextModelSnapshot : ModelSnapshot
+    [Migration("20241130045705_AgregarOrdenDeCompra")]
+    partial class AgregarOrdenDeCompra
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241130045705_AgregarOrdenDeCompra.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaComprasMVC.Migrations
+{
+    public partial class AgregarOrdenDeCompra : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrdenesDeCompra",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NumeroOrden = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaOrden = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false),
+                    ArticuloId = table.Column<int>(type: "int", nullable: false),
+                    UnidadDeMedidaId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    CostoUnitario = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdenesDeCompra", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrdenesDeCompra_Articulos_ArticuloId",
+                        column: x => x.ArticuloId,
+                        principalTable: "Articulos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrdenesDeCompra_UnidadesDeMedida_UnidadDeMedidaId",
+                        column: x => x.UnidadDeMedidaId,
+                        principalTable: "UnidadesDeMedida",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdenesDeCompra_ArticuloId",
+                table: "OrdenesDeCompra",
+                column: "ArticuloId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdenesDeCompra_UnidadDeMedidaId",
+                table: "OrdenesDeCompra",
+                column: "UnidadDeMedidaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdenesDeCompra");
+        }
+    }
+}
